f1df7b0cb8bab42091130b6f35cd0c7b09bcc958
isjulianJaywing <julian.jackson@jaywing.com>
2019-01-22 23:17:57 +0000

    update

--- app.js
+++ app.js
@@ -6,25 +6,22 @@ var sensorId = "28-00000a69fc4b";
 var serverFunction =
   "https://kingsbridgetemperature.azurewebsites.net/api/StoreTemperatureData?code=9HCPEi8fV5arG97GyyYfzOBaN7UzyYsWateAh5qOka55JFUZxrwmkQ==";
 
-var tempTimeTaken;
-var data = {};
-
 sensor.get(sensorId, function(err, temp) {
   if (err) {
     console.log(err);
     return;
-  } else {
-    tempTimeTaken = new Date()
-      .toISOString()
-      .slice(0, 19)
-      .replace("T", " ");
-    return (data = {
-      temp: temp,
-      sensor: sensorId,
-      date: tempTimeTaken
-    });
   }
 });
+var temp = sensor.get(sensorId);
+var tempTimeTaken = new Date()
+  .toISOString()
+  .slice(0, 19)
+  .replace("T", " ");
+var data = {
+  temp: temp,
+  sensor: sensorId,
+  date: tempTimeTaken
+};
 console.log(data);
 xhr.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
