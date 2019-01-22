de0dfc7f7f0249c972e9e367a1d0d7bb97078682
isjulianJaywing <julian.jackson@jaywing.com>
2019-01-22 22:28:25 +0000

    updated func for rasp

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-var mysql = require('mysql');
-
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
+var sensor = require("ds18x20");
+var sensorId = "28-00000a69fc4b";
 
 var serverFunction =
-  'https://kingsbridgetemperature.azurewebsites.net/api/StoreTemperatureData?code=9HCPEi8fV5arG97GyyYfzOBaN7UzyYsWateAh5qOka55JFUZxrwmkQ==';
+  "https://kingsbridgetemperature.azurewebsites.net/api/StoreTemperatureData?code=9HCPEi8fV5arG97GyyYfzOBaN7UzyYsWateAh5qOka55JFUZxrwmkQ==";
 
-var tempTimeTaken = new Date();
+var temperature;
+var tempTimeTaken;
+var data = {};
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log('Connected!');
+sensor.get(sensorId, function(err, temp) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  tempTimeTaken = new Date()
+    .toISOString()
+    .slice(0, 19)
+    .replace("T", " ");
+  temperature = temp;
+
+  data.temp = temp;
+  data.sensor = sensorId;
+  data.date = date;
 });
-
-var data = {
-  temp: 90.3,
-  sensor: 'dans willy',
-  date: tempTimeTaken
-};
 
 xhr.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     console.log(this.responseText);
-  } else {
-    console.log(this.responseText);
   }
 };
-xhr.open('POST', serverFunction);
-xhr.setRequestHeader('Content-type', 'application/JSON');
+xhr.open("POST", serverFunction);
+xhr.setRequestHeader("Content-type", "application/JSON");
 xhr.send(JSON.stringify(data));
