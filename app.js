448d704245407070a5e37d6f0103a6b07c33590e
isjulianJaywing <julian.jackson@jaywing.com>
2019-01-22 07:34:12 +0000

    mysql instead

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var mysql = require("mysql");
 
 var xhr = new XMLHttpRequest();
 
-var serverFunction =
-  'https://kingsbridgetemp.azurewebsites.net/api/HttpTrigger1?code=KfPpR4uOu3aTmIGXeyWzOniSE/Bicz712bGSvK0NkN/6MZW3vbMAkA==';
-
 var tempTimeTaken = new Date();
+
+var con = mysql.createConnection({
+  host: "db770327330.hosting-data.io",
+  user: "dbo770327330",
+  password: "",
+  database: "db770327330",
+  port: 3306
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 var data = {
   temp: 90.3,
-  sensor: 'dans willy',
+  sensor: "dans willy",
   date: tempTimeTaken
 };
 
-xhr.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    console.log(this.responseText);
-  }
-};
-xhr.open('POST', serverFunction);
-xhr.setRequestHeader('Content-type', 'application/JSON');
-xhr.send(JSON.stringify(data));
+// xhr.onreadystatechange = function() {
+//   if (this.readyState == 4 && this.status == 200) {
+//     console.log(this.responseText);
+//   }
+// };
+// xhr.open("POST", serverFunction);
+// xhr.setRequestHeader("Content-type", "application/JSON");
+// xhr.send(JSON.stringify(data));
